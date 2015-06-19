6baba062852e768fd73497a85125827275bee38a
Jadon Leininger <jleininger@student.neumont.edu>
2015-06-19 15:58:18 -0600

    Added manifest, icons, and changed to using React.js withouth JSX

--- Assets/Scripts/script.jsx
+++
@@ -1,9 +0,0 @@
-var Main = React.createClass({
-    render: function () {
-        return ( 
-            <h1>Hello! Im rendered from React.js!</h1>
-            )
-        }
-});
-
-React.render( <Main />, document.getElementById("container"));
