8ca7029765f8458cc7eac784f22e4c868669f7dc
Jadon Leininger <jleininger@student.neumont.edu>
2015-06-21 16:48:34 -0600

    Working on adding photos to the collection

--- Assets/Scripts/background.js
+++ Assets/Scripts/background.js
@@ -1,8 +1,23 @@
 var bookmarks = {};
 
-bookmarks.bookmarkPage = function(info) {
-    chrome.tabs.query({ currentWindow: true. active: true }, function(tabs) {
 
+
+bookmarks.createPageBookmark = function(pageUrl, pageTitle) {
+    var pageName = window.prompt("Name this page.", pageTitle),
+        page = {
+            key: pageName,
+            value: pageUrl
+        };
+    return page;
+};
+
+bookmarks.bookmarkPage = function(info) {
+    chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+        //Create the new page
+        var currentTab = tabs[0],
+            page = bookmarks.createPageBookmark(info.pageUrl, currentTab.title);
+        //Save the page to the database
+        chrome.storage
     });
 };
 
