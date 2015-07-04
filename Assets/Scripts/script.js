1ca44bb991fda5b0d03672532298f996b38424ff
Jadon Leininger <jleininger@student.neumont.edu>
2015-07-04 16:13:32 -0600

    Delete working for pages, photos, and videos.

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -3,13 +3,14 @@ var Page = React.createClass({
     render: function() {
         var title = this.props.title,
             url = this.props.url,
+            index = this.props.index,
             openUrl = this.props.openUrl,
             dragBookmark = this.props.dragBookmark,
             dropBookmark = this.props.dropBookmark;
 
         return React.createElement(
             'div',
-            { className: 'main-menu', onClick: openUrl.bind(null, url), draggable: true, onDragStart: dragBookmark, onDragEnd: dropBookmark },
+            { className: 'main-menu', onClick: openUrl.bind(null, url), draggable: true, onDragStart: dragBookmark.bind(null, index), onDragEnd: dropBookmark },
             React.createElement('h2', null, title)
         );
     }
@@ -19,9 +20,12 @@ var Photo = React.createClass({
     displayName: 'Photo',
     render: function() {
         var url = this.props.url,
-            openUrl = this.props.openUrl;
+            index = this.props.index,
+            openUrl = this.props.openUrl,
+            dragBookmark = this.props.dragBookmark,
+            dropBookmark = this.props.dropBookmark;
 
-        return React.createElement('img', {className: 'photo', src: url, onClick: openUrl.bind(null, url)}, null);
+        return React.createElement('img', {className: 'photo', src: url, onClick: openUrl.bind(null, url), draggable: true, onDragStart: dragBookmark.bind(null, index), onDragEnd: dropBookmark}, null);
     }
 });
 
@@ -29,10 +33,14 @@ var Video = React.createClass({
     displayName: 'Video',
     render: function() {
         var url = this.props.url,
-            openUrl = this.props.openUrl;
+            index = this.props.index,
+            openUrl = this.props.openUrl,
+            dragBookmark = this.props.dragBookmark,
+            dropBookmark = this.props.dropBookmark;
+
         return React.createElement(
             'video',
-            { className: 'video', controls: true, onClick: openUrl.bind(null, url)},
+            { className: 'video', controls: true, onClick: openUrl.bind(null, url), draggable: true, onDragStart: dragBookmark.bind(null, index), onDragEnd: dropBookmark},
             React.createElement('source', { src: url}, null)
         )
     }
@@ -70,22 +78,12 @@ var Main = React.createClass({
 
         return pageType.toUpperCase();
     },
-    dragBookmark: function() {
-        console.log('Dragging the bookmark');
+    dragBookmark: function(index, e) {
+        e.dataTransfer.setData('index', index);
         this.state.header.style.display = 'block';
     },
     dropBookmark: function() {
         this.state.header.style.display = 'none';
-    },
-    deleteBookmark: function(e) {
-        var confirmDelete = window.confirm('Are you sure you want to remove this bookmark?');
-
-        if(confirmDelete) {
-            //Delete the bookmark
-        }
-    },
-    openUrl: function(url) {
-        chrome.runtime.sendMessage({action: 'openUrl', url: url});
     },
     getBookmarks: function(bookmarkType, next) {
         chrome.storage.sync.get(bookmarkType, function(result) {
@@ -98,6 +96,23 @@ var Main = React.createClass({
             next(bookmarks);
         });
     },
+    deleteBookmark: function(e) {
+        var confirmDelete = window.confirm('Are you sure you want to remove this bookmark?'),
+            index = e.dataTransfer.getData('index');
+
+        if(confirmDelete) {
+            if(this.state.pageType === 'PAGES') {
+                this.deletePage(index);
+            } else if(this.state.pageType === 'PHOTOS') {
+                this.deletePhoto(index);
+            } else if(this.state.pageType === 'VIDEOS') {
+                this.deleteVideo(index);
+            }
+        }
+    },
+    openUrl: function(url) {
+        chrome.runtime.sendMessage({action: 'openUrl', url: url});
+    },
     getPages: function() {
         var context = this,
             pages = [];
@@ -107,13 +122,24 @@ var Main = React.createClass({
                 var b = bookmarks[i];
                 pages.push(React.createElement(
                     Page,
-                    {title: b.key, url: b.value, openUrl: context.openUrl, dragBookmark: context.dragBookmark, dropBookmark: context.dropBookmark}
+                    {title: b.key, url: b.value, index: i, openUrl: context.openUrl, dragBookmark: context.dragBookmark, dropBookmark: context.dropBookmark}
                 ));
             }
             context.setState({
                 pages: pages,
                 pageToRender: React.createElement('div', null, pages)
             });
+        });
+    },
+    deletePage: function(index) {
+        //Remove React components
+        var pages = this.state.pages;
+        this.setState( {pages: pages.splice(index, 1)});
+
+        //Remove bookmarks in storage
+        this.getBookmarks('Pages', function(bookmarks) {
+            bookmarks.splice(index, 1);
+            chrome.storage.sync.set({'Pages': bookmarks});
         });
     },
     getPhotos: function() {
@@ -123,12 +149,23 @@ var Main = React.createClass({
         this.getBookmarks('Photos', function(bookmarks) {
             for(var i = 0; i < bookmarks.length; i++) {
                 var b = bookmarks[i];
-                photos.push(React.createElement(Photo, {url: b, openUrl: context.openUrl}));
+                photos.push(React.createElement(Photo, {url: b, index: i, openUrl: context.openUrl, dragBookmark: context.dragBookmark, dropBookmark: context.dropBookmark}));
             }
             context.setState({
                 photos: photos,
                 pageToRender: React.createElement('div', null, photos)
             });
+        });
+    },
+    deletePhoto: function(index) {
+        //Remove React components
+        var photos = this.state.photos;
+        this.setState( {pages: photos.splice(index, 1)});
+
+        //Remove bookmarks in storage
+        this.getBookmarks('Photos', function(bookmarks) {
+            bookmarks.splice(index, 1);
+            chrome.storage.sync.set({'Photos': bookmarks});
         });
     },
     getVideos: function() {
@@ -138,12 +175,23 @@ var Main = React.createClass({
         this.getBookmarks('Videos', function(bookmarks) {
             for(var i = 0; i < bookmarks.length; i++) {
                 var b = bookmarks[i];
-                videos.push(React.createElement(Video, {url: b, openUrl: context.openUrl}));
+                videos.push(React.createElement(Video, {url: b, index: i, openUrl: context.openUrl, dragBookmark: context.dragBookmark, dropBookmark: context.dropBookmark}));
             }
             context.setState({
                 videos: videos,
                 pageToRender: React.createElement('div', null, videos)
             });
+        });
+    },
+    deleteVideo: function(index) {
+        //Remove React components
+        var videos = this.state.videos;
+        this.setState( {pages: videos.splice(index, 1)});
+
+        //Remove bookmarks in storage
+        this.getBookmarks('Videos', function(bookmarks) {
+            bookmarks.splice(index, 1);
+            chrome.storage.sync.set({'Videos': bookmarks});
         });
     },
     render: function() {
