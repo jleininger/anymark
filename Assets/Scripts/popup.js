eaa06a4a918d0a9bc69497512d37c4c8d4802f9c
Jadon Leininger <jleininger@student.neumont.edu>
2015-07-14 10:50:14 -0600

    Incogito on popup page working on first open now. All styling changed as well. Now need to implement better method into script.js

--- Assets/Scripts/popup.js
+++ Assets/Scripts/popup.js
@@ -1,13 +1,12 @@
 window.onload = function() {
-    if(chrome.extension.getBackgroundPage().bookmarks.pageInfo.isIncognito) {
-        document.getElementById('mainHeader').style.backgroundColor = "#191919";
-        document.getElementsByTagName('footer')[0].style.backgroundColor = "#191919";
-        var menuElements = document.getElementsByClassName('main-menu');
-        document.get
-        console.log(menuElements);
-        //TODO: Need to figure out a way to get all the elements and
-        //for(var i = 0; i < menuElements.length; i++) {
-        //    menuElements[i].className = "main-menu-incognito";
-        //}
-    }
+    chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+        if (tabs[0].incognito) {
+            document.getElementById('mainHeader').style.backgroundColor = "#191919";
+            document.getElementsByTagName('footer')[0].style.backgroundColor = "#191919";
+            document.getElementById('pagesMenu').className = "main-menu-incognito";
+            document.getElementById('photosMenu').className = "main-menu-incognito";
+            document.getElementById('videosMenu').className = "main-menu-incognito";
+            document.getElementById('settingsMenu').className = "main-menu-incognito";
+        }
+    });
 };
